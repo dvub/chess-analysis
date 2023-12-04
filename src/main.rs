f77924214768f73a93a14df0f727ec6c7d7c2273
dvub <dvubdevs@gmail.com>
2023-12-03 20:59:51 -0800

    this code is a mess but this works.

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,13 @@
 use std::{collections::HashMap, fs::File, io::BufReader};
 
-use pgn_reader::{BufferedReader, SanPlus, Skip};
+use pgn_reader::{BufferedReader, SanPlus, Skip, Visitor};
 use plotters::prelude::*;
 struct GameReader {
     moves: usize,
     total_games: usize,
     all_times: Vec<i32>,
-    map: HashMap<i32, Vec<i32>>,
+    time_map: HashMap<i32, Vec<i32>>,
+    time_control_offset: i32,
 }
 impl GameReader {
     fn new() -> GameReader {
@@ -14,12 +15,13 @@ impl GameReader {
             moves: 0,
             total_games: 0,
             all_times: Vec::new(),
-            map: HashMap::new(),
+            time_map: HashMap::new(),
+            time_control_offset: 0,
         }
     }
 }
 
-impl pgn_reader::Visitor for GameReader {
+impl Visitor for GameReader {
     type Result = usize;
 
     fn begin_game(&mut self) {
@@ -35,6 +37,16 @@ impl pgn_reader::Visitor for GameReader {
         Skip(true) // stay in the mainline
     }
 
+    fn header(&mut self, key: &[u8], value: pgn_reader::RawHeader<'_>) {
+        let str = std::str::from_utf8(key).unwrap();
+        if str == "TimeControl" {
+            let value_str = std::str::from_utf8(value.0).unwrap();
+
+            let offset = value_str.split('+').nth(1).unwrap();
+            self.time_control_offset = offset.parse().unwrap();
+        }
+    }
+
     // once a game is over, we want to use all the times we collected
     fn end_game(&mut self) -> Self::Result {
         // if the game had no moves or something like that, take care of it here
@@ -46,11 +58,11 @@ impl pgn_reader::Visitor for GameReader {
 
         //
         for time in self.all_times.iter().step_by(2) {
-            let remaining_time = time;
-            let delta_time = last_time - time;
+            let remaining_time = *time;
+            let delta_time = last_time - (time - self.time_control_offset);
 
-            self.map
-                .entry(*remaining_time)
+            self.time_map
+                .entry(remaining_time)
                 .or_default()
                 .push(delta_time);
 
@@ -95,7 +107,7 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
     let current = std::env::current_dir()?;
 
     // get the correct game file within games directory
-    let file_name = "games/some-games.pgn";
+    let file_name = "games/sample.pgn";
     let file = current.join(file_name);
 
     // open
@@ -112,31 +124,32 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
         game_reader.total_games
     );
 
-    println!("{:?}", game_reader.map);
+    println!("{:?}", game_reader.time_map);
 
-    let root = BitMapBackend::new("plotters-doc-data/0.png", (640, 480)).into_drawing_area();
+    let root = BitMapBackend::new("graphs/0.png", (1920, 1080)).into_drawing_area();
     root.fill(&WHITE)?;
     let mut chart = ChartBuilder::on(&root)
-        .caption("y=x^2", ("sans-serif", 50).into_font())
+        // .caption("y=x^2", ("sans-serif", 50).into_font())
         .margin(5)
         .x_label_area_size(30)
         .y_label_area_size(30)
-        .build_cartesian_2d(-1f32..1f32, -0.1f32..1f32)?;
+        .build_cartesian_2d(0f32..670f32, 0f32..670f32)?;
 
     chart.configure_mesh().draw()?;
-
     chart
-        .draw_series(LineSeries::new(
-            (-50..=50).map(|x| x as f32 / 50.0).map(|x| (x, x * x)),
-            &RED,
-        ))?
-        .label("y = x^2")
-        .legend(|(x, y)| PathElement::new(vec![(x, y), (x + 20, y)], &RED));
+        .draw_series(game_reader.time_map.iter().map(|(x, y_values)| {
+            Circle::new(
+                (*x as f32, *y_values.first().unwrap() as f32),
+                2,
+                GREEN.filled(),
+            )
+        }))
+        .unwrap();
 
     chart
         .configure_series_labels()
-        .background_style(&WHITE.mix(0.8))
-        .border_style(&BLACK)
+        .background_style(WHITE.mix(0.8))
+        .border_style(BLACK)
         .draw()?;
 
     root.present()?;
